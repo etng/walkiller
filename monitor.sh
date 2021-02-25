b52fb40ba4444f27f4cea1ee9f20dd3817f45afd
Yi Bo <etng2004@gmail.com>
2021-02-25 17:35:30 +0800

    add dingtalk notify

--- monitor.sh
+++ monitor.sh
@@ -14,7 +14,16 @@ do
         if [ $? -ne 0 ];
         then
             echo "$(date) fuck, no ok"
-            curl -X POST https://sc.ftqq.com/${SC_KEY}.send --form "text=主人：项目代理挂啦${POLIPO_PORT}" --form "desp=代理挂啦${POLIPO_PORT} ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}"
+            if [ -n "${SC_KEY}" ]; then
+                echo "notify with ftqq"
+                curl -X POST https://sc.ftqq.com/${SC_KEY}.send --form "text=主人：项目代理挂啦${POLIPO_PORT}" --form "desp=代理挂啦${POLIPO_PORT} ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}"
+            fi
+            if [ -n "${DINGTALK_TOKEN}" ]; then
+                echo "notify with dingding"
+                curl "https://oapi.dingtalk.com/robot/send?access_token=${DINGTALK_TOKEN}" \
+                -H 'Content-Type: application/json' \
+                -d "{\"msgtype\": \"text\",\"text\": {\"content\": \"${DINGTALK_PREFIX} ${POLIPO_PORT}代理挂啦 ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}\"}}"
+            fi
         else
             echo "$(date) proxy back online now"
         fi
@@ -24,3 +33,10 @@ do
     sleep 120
 done
 echo "$(date) proxy monitor exited"
+
+export DINGTALK_TOKEN=4a6e577b5aae5b234589d625508e848dbd74714689c471fbec784b2c5e0eaefe
+export DINGTALK_PREFIX="大喇叭说："
+
+
+
+
