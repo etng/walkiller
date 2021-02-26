d1360876d9a03525d756ac6e214853f2c05b2f3b
Yi Bo <etng2004@gmail.com>
2021-02-26 10:44:55 +0800

    machine name and timestamp in notify

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+machine=`cat /etc/host_name`
 echo "$(date) proxy monitor started"
+echo "$(date) ${machine} wait for first check"
+sleep 30
 while :
 do
     echo "$(date) proxy checking at port ${POLIPO_PORT}"
@@ -7,6 +10,7 @@ do
     if [ $? -ne 0 ];
     then
         echo "$(date) proxy down trying restart"
+        now=`date '+%Y-%m-%d %H:%M:%S'`
         pkill ss-local
         pkill polipo
         sleep 5
@@ -16,13 +20,13 @@ do
             echo "$(date) fuck, no ok"
             if [ -n "${SC_KEY}" ]; then
                 echo "notify with ftqq"
-                curl -X POST https://sc.ftqq.com/${SC_KEY}.send --form "text=主人：项目代理挂啦${POLIPO_PORT}" --form "desp=代理挂啦${POLIPO_PORT} ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}"
+                curl -X POST https://sc.ftqq.com/${SC_KEY}.send --form "text=主人：项目代理挂啦${POLIPO_PORT}" --form "desp=${machine} ${now} 代理挂啦${POLIPO_PORT} ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}"
             fi
             if [ -n "${DINGTALK_TOKEN}" ]; then
                 echo "notify with dingding"
                 curl "https://oapi.dingtalk.com/robot/send?access_token=${DINGTALK_TOKEN}" \
                 -H 'Content-Type: application/json' \
-                -d "{\"msgtype\": \"text\",\"text\": {\"content\": \"${DINGTALK_PREFIX} ${POLIPO_PORT}代理挂啦 ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}\"}}"
+                -d "{\"msgtype\": \"text\",\"text\": {\"content\": \"${DINGTALK_PREFIX} ${machine} ${now} ${POLIPO_PORT}代理挂啦 ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}\"}}"
             fi
         else
             echo "$(date) proxy back online now"
