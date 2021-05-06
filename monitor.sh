c485ae8ccdb15c40c48cb2ce7bd54979b9d3c9f7
Yi Bo <etng2004@gmail.com>
2021-05-06 17:26:53 +0800

    notify check

--- monitor.sh
+++ monitor.sh
@@ -6,7 +6,7 @@ sleep 30
 while :
 do
     echo "$(date) proxy checking at port ${POLIPO_PORT}"
-    curl -x http://127.0.0.1:${POLIPO_PORT} http://ip.fm --fail
+    curl -sSL -x http://127.0.0.1:${POLIPO_PORT} http://ip.fm --fail
     if [ $? -ne 0 ];
     then
         echo "$(date) proxy down trying restart"
@@ -14,17 +14,17 @@ do
         pkill ss-local
         pkill polipo
         sleep 5
-        curl -x http://127.0.0.1:${POLIPO_PORT} http://ip.fm --fail
+        curl -sSL -x http://127.0.0.1:${POLIPO_PORT} http://ip.fm --fail
         if [ $? -ne 0 ];
         then
             echo "$(date) fuck, no ok"
             if [ -n "${SC_KEY}" ]; then
                 echo "notify with ftqq"
-                curl -X POST https://sc.ftqq.com/${SC_KEY}.send --form "text=主人：项目代理挂啦${POLIPO_PORT}" --form "desp=${machine} ${now} 代理挂啦${POLIPO_PORT} ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}"
+                curl -sSL -X POST https://sc.ftqq.com/${SC_KEY}.send --form "text=主人：项目代理挂啦${POLIPO_PORT}" --form "desp=${machine} ${now} 代理挂啦${POLIPO_PORT} ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}"
             fi
             if [ -n "${DINGTALK_TOKEN}" ]; then
                 echo "notify with dingding"
-                curl "https://oapi.dingtalk.com/robot/send?access_token=${DINGTALK_TOKEN}" \
+                curl -sSL "https://oapi.dingtalk.com/robot/send?access_token=${DINGTALK_TOKEN}" \
                 -H 'Content-Type: application/json' \
                 -d "{\"msgtype\": \"text\",\"text\": {\"content\": \"${DINGTALK_PREFIX} ${machine} ${now} ${POLIPO_PORT}代理挂啦 ${SS_METHOD}://${SS_PASSWORD}@${SS_SERVER}:${SS_SERVER_PORT}\"}}"
             fi
